720ee574fade07c6ea9b9d869b111dd1756412e7
Emir <107417506+emirow@users.noreply.github.com>
2026-01-07 15:18:35 +0500

    Create mysite.js

---
+++ mysite.js
@@ -0,0 +1,8 @@
+    const dots = document.getElementById("dots");
+    const frames = ["", ".", "..", "..."];
+    let i = 0;
+
+    setInterval(() => {
+      dots.textContent = frames[i];
+      i = (i + 1) % frames.length;
+    }, 400);
