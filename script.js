7d8882759f8e66e0ff9c4b38da3c13f6d1803c20
Emir <107417506+emirow@users.noreply.github.com>
2026-03-12 00:59:33 +0500

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,8 @@
       dots.textContent = frames[i];
       i = (i + 1) % frames.length;
     }, 400);
+
+    let timeUP = function() {
+      alert("This site is currently being updated!");
+    }
+    setTimeout(timeUP, 3000);
